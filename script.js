bc74cb9ac4b59a9cfa6154641f156f3c33ccb595
VIJAYKUMAR2435 <vijaykumar993940@gmail.com>
2024-07-24 12:28:51 +0530

    new commit1

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function secondsToMinuteSeconds(seconds, useDoubleColons = false) {
 
 async function getSongs(folder){
   currFolder=folder;
-let a=await fetch(`/${folder}/`)
+let a=await fetch(`./${folder}/`)
 let response=await a.text();
 
 let div=document.createElement("div")
